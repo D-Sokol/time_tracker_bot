07096dc7e679428e4a52b00908afb358934b0354
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2019-09-10 11:26:37 +0300

    Realized echo bot

--- time_tracker.py
+++ time_tracker.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python3
+import os
+import telebot
 
+bot = telebot.TeleBot(os.environ.get('TOKEN'))
+
+# FIXME: it is possible to have 0 filters?
+@bot.message_handler()
+def default_message_handler(message):
+    # it shows structure of message object
+    bot.reply_to(message, str(message.__dict__))
+
+
+bot.polling()
