9c30966e3b195a81c8af83a1f0199009e8e49e24
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2019-09-14 17:49:22 +0300

    add actions for main commands

--- management.py
+++ management.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from .models import session
 from .models import User, Record
 
@@ -23,6 +25,7 @@ def create_record(user_id, begin_time, end_time):
     record = Record(user_id=user_id, begin_time=begin_time, end_time=end_time)
     session.add(record)
     session.commit()
+    return record
 
 
 def delete_record(record_id):
@@ -30,3 +33,22 @@ def delete_record(record_id):
     if record:
         session.delete(record)
         session.commit()
+
+
+# Functions, realizing bot's commands
+def begin_interval(user_id, time=None):
+    user = ensure_user(user_id)
+    if time is None:
+        time = datetime.now()
+    user.current_start_time = time
+    session.commit()
+
+
+def end_interval(user_id, end_time=None):
+    user = ensure_user(user_id)
+    start_time = user.current_start_time
+    if start_time is None:
+        # TODO: custom exceptions
+        raise ValueError('Cannot end interval without start')
+    user.current_start_time = None
+    return create_record(user_id, start_time, end_time or datetime.now())
