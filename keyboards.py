1420cdf40d18ed4a791b68ecaf6c7d818e49dd60
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2019-10-24 21:28:06 +0300

    Fixed keyboards settings

--- keyboards.py
+++ keyboards.py
@@ -1,12 +1,13 @@
 from telebot import types
 
 
-keyboard_notstarted = types.ReplyKeyboardMarkup()
+keyboard_notstarted = types.ReplyKeyboardMarkup(row_width=2)
 keyboard_notstarted.row('Begin interval')
 keyboard_notstarted.row('Last record', 'File', 'My timezone')
 
 
-keyboard_started = types.ReplyKeyboardMarkup()
+keyboard_started = types.ReplyKeyboardMarkup(row_width=2)
 keyboard_started.row('End interval', 'Cancel interval')
-keyboard_notstarted.row('Last record', 'File', 'My timezone')
+keyboard_started.row('Last record', 'File', 'My timezone')
+
 
